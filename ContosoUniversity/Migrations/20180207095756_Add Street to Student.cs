779d3c94f682b199b87eedaf4f45bfbd05017d4e
Martin Dätz <Martin.Daetz@vanderlande.com>
2018-02-08 09:54:55 +0100

    add Docker Container postgress

---
+++ ContosoUniversity/Migrations/20180207095756_Add Street to Student.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class AddStreettoStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Student",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Student");
+        }
+    }
+}
